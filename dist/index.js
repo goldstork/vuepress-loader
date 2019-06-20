da9e901267cdaf8d8c2628ed030e9d51208f49bd
IERomanov <i.e.romanov1997@gmail.com>
2019-06-20 21:27:41 +0400

    try fix filename and filecontent 'is required' error

--- dist/index.js
+++ dist/index.js
@@ -25,60 +25,51 @@ var _options = _interopRequireDefault(require("./options.json"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function loader(source) {
-  const spinner = (0, _ora.default)('Generate docs').start();
-  spinner.color = 'green';
-  const options = _loaderUtils.default.getOptions(this) || {};
-  (0, _schemaUtils.default)(_options.default, options, 'VuePress Loader');
-  const context = options.context || this.rootContext;
+  try {
+    const spinner = (0, _ora.default)('Generate docs').start();
+    spinner.color = 'green';
+    const options = _loaderUtils.default.getOptions(this) || {};
+    (0, _schemaUtils.default)(_options.default, options, 'VuePress Loader');
+    const context = options.context || this.rootContext;
 
-  const url = _loaderUtils.default.interpolateName(this, options.name, {
-    context,
-    source,
-    regExp: options.regExp
-  });
+    const url = _loaderUtils.default.interpolateName(this, options.name, {
+      context,
+      content: source.toString('utf8'),
+      regExp: options.regExp
+    });
 
-  let outputPath = url;
+    let outputPath = url;
 
-  if (options.outputPath) {
-    if (typeof options.outputPath === 'function') {
-      outputPath = options.outputPath(url, this.resourcePath, context);
-    } else {
-      outputPath = _path.default.posix.join(options.outputPath, url);
+    if (options.outputPath) {
+      if (typeof options.outputPath === 'function') {
+        outputPath = options.outputPath(url, this.resourcePath, context);
+      } else {
+        outputPath = _path.default.posix.join(options.outputPath, url);
+      }
     }
-  }
 
-  const componentData = (0, _parser.parse)(source.toString('utf8'));
+    const componentData = (0, _parser.parse)(source.toString('utf8'));
 
-  const writeable = _fs.default.createWriteStream(JSON.stringify(outputPath));
+    const writeable = _fs.default.createWriteStream(JSON.stringify(outputPath));
 
-  writeable.on('error', err => {
+    writeable.on('error', err => {
+      process.stdout.cursorTo(0);
+      process.stdout.write(err);
+      spinner.fail('Docs generation faild!!!');
+    });
+    writeable.on('end', () => {
+      process.stdout.cursorTo(0);
+      spinner.succeed('Successfully generate docs!');
+    });
+    const componentKeys = [];
+    JSON.parse(componentData).keys().forEach(key => componentKeys.push(key));
+    componentKeys.forEach(key => writeable.write(`${key}\n`));
+    writeable.end();
+  } catch (err) {
     process.stdout.cursorTo(0);
     process.stdout.write(err);
-    spinner.fail('Docs generation faild!!!');
-  });
-  writeable.on('end', () => {
-    process.stdout.cursorTo(0);
-    process.stdout.write('Successfully generate docs!');
-    spinner.succeed('Successfully generate docs!');
-  });
-  const componentKeys = [];
-  JSON.parse(componentData).keys().forEach(key => componentKeys.push(key));
-  componentKeys.forEach(key => writeable.write(`${key}\n`));
-  writeable.end(); // let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`
-  // if (options.publicPath) {
-  // 	if (typeof options.publicPath === 'function') {
-  // 		publicPath = options.publicPath(url, this.resourcePath, context)
-  // 	} else {
-  // 		publicPath = `${
-  // 			options.publicPath.endsWith('/') ? options.publicPath : `${options.publicPath}/`
-  // 		}${url}`
-  // 	}
-  // 	publicPath = JSON.stringify(publicPath)
-  // }
-  //   if (typeof options.emitFile === 'undefined' || options.emitFile) {
-  //     this.emitFile(outputPath, source);
-  //   }
-  //   return `module.exports = ${publicPath};`;
+    spinner.fail('Docs generation faild!');
+  }
 }
 
 const raw = true;
--- src/index.js
+++ src/index.js
@@ -10,71 +10,57 @@ import validateOptions from 'schema-utils'
 import schema from './options.json'
 
 export default function loader(source) {
-	const spinner = ora('Generate docs').start();
-	spinner.color = 'green'
-	const options = loaderUtils.getOptions(this) || {}
-
-	validateOptions(schema, options, 'VuePress Loader')
-
-	const context = options.context || this.rootContext
-
-	const url = loaderUtils.interpolateName(this, options.name, {
-		context,
-		source,
-		regExp: options.regExp,
-	})
-
-	let outputPath = url
-
-	if (options.outputPath) {
-		if (typeof options.outputPath === 'function') {
-			outputPath = options.outputPath(url, this.resourcePath, context)
-		} else {
-			outputPath = path.posix.join(options.outputPath, url)
-		}
-	}
+	try {
+		const spinner = ora('Generate docs').start();
+		spinner.color = 'green'
 	
-	const componentData = parse(source.toString('utf8'))
-
-	const writeable = fs.createWriteStream(JSON.stringify(outputPath))
-
-	writeable.on('error', err => {
+		const options = loaderUtils.getOptions(this) || {}
+	
+		validateOptions(schema, options, 'VuePress Loader')
+	
+		const context = options.context || this.rootContext
+	
+		const url = loaderUtils.interpolateName(this, options.name, {
+			context,
+			content: source.toString('utf8'),
+			regExp: options.regExp,
+		})
+	
+		let outputPath = url
+	
+		if (options.outputPath) {
+			if (typeof options.outputPath === 'function') {
+				outputPath = options.outputPath(url, this.resourcePath, context)
+			} else {
+				outputPath = path.posix.join(options.outputPath, url)
+			}
+		}
+		
+		const componentData = parse(source.toString('utf8'))
+	
+		const writeable = fs.createWriteStream(JSON.stringify(outputPath))
+	
+		writeable.on('error', err => {
+			process.stdout.cursorTo(0);
+			process.stdout.write(err);
+			spinner.fail('Docs generation faild!!!')
+		})
+	
+		writeable.on('end', () => {
+			process.stdout.cursorTo(0);
+			spinner.succeed('Successfully generate docs!')
+		})
+	
+		const componentKeys = []
+		JSON.parse(componentData).keys().forEach(key => componentKeys.push(key))
+		
+		componentKeys.forEach(key => writeable.write(`${key}\n`));
+		writeable.end()
+	} catch (err) {
 		process.stdout.cursorTo(0);
 		process.stdout.write(err);
-		spinner.fail('Docs generation faild!!!')
-	})
-
-	writeable.on('end', () => {
-		process.stdout.cursorTo(0);
-		process.stdout.write('Successfully generate docs!');
-		spinner.succeed('Successfully generate docs!')
-	})
-
-	const componentKeys = []
-	JSON.parse(componentData).keys().forEach(key => componentKeys.push(key))
-	
-	componentKeys.forEach(key => writeable.write(`${key}\n`));
-	writeable.end()
-
-	// let publicPath = `__webpack_public_path__ + ${JSON.stringify(outputPath)}`
-
-	// if (options.publicPath) {
-	// 	if (typeof options.publicPath === 'function') {
-	// 		publicPath = options.publicPath(url, this.resourcePath, context)
-	// 	} else {
-	// 		publicPath = `${
-	// 			options.publicPath.endsWith('/') ? options.publicPath : `${options.publicPath}/`
-	// 		}${url}`
-	// 	}
-
-	// 	publicPath = JSON.stringify(publicPath)
-	// }
-
-	//   if (typeof options.emitFile === 'undefined' || options.emitFile) {
-	//     this.emitFile(outputPath, source);
-	//   }
-
-	//   return `module.exports = ${publicPath};`;
+		spinner.fail('Docs generation faild!')
+	}
 }
 
 export const raw = true
